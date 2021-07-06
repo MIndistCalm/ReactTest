121fcd0b0424457aadb0f308b8165cba9c6042a5
dannotdead <daniil.koka2000@gmail.com>
2021-07-06 19:16:48 +0700

    create article form

--- src/components/workWithContent/CreateContent.js
+++ src/components/workWithContent/CreateContent.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Button, Form} from "react-bootstrap";
 
 const CreateContent = () => {
   return (
-    <div>
-      <h1>Create Form</h1>
-    </div>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Создать статью</Form.Label>
+        <Form.Control type="text" placeholder="Создать статью" />
+        <Form.Text className="text-muted">
+          Создать статью
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Добавить описание</Form.Label>
+        <Form.Control type="text" placeholder="Добавить описание" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Подтвердить" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Подтвердить
+      </Button>
+    </Form>
   );
 };
 
