f6f61fc275145688d65e2f46e88191025572d8ac
dannotdead <daniil.koka2000@gmail.com>
2021-07-08 12:30:28 +0700

    удаление из localstorage

--- src/components/workWithContent/Articles.js
+++ src/components/workWithContent/Articles.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import Pagination from "./Pagination";
 import {Figure} from "react-bootstrap";
 import CreateArticle from '../../static/plus-square.svg'
+import {articles} from '../../store/ArticleStore'
 
 const Articles = () => {
 
@@ -11,59 +12,26 @@ const Articles = () => {
   const [articlesPerPage, setArticlesPerPage] = useState(10);
 
   const deleteItem = (id) => {
-        setItems(itemsMas.filter(article => article.id !== id))
-    }
-
-  const articles = [
-      {
-        id: 1,
-        title: 'Кастрация кота',
-        description:
-          'Кастрация кота – это искусственное прекращение функции половых желёз, ' +
-          'чаще всего при этой операции удаляют оба семенника.\n ' +
-          'Во многих странах эта операция является обязательной для неплеменных животных, ' +
-          'так называемого, пет-класса (домашних любимцев).\n',
-        category: 'Cat'
-      },
-      {
-        id: 2,
-        title: 'Летний уход за бесшерстной собакой',
-        description:
-          'Чем больше у собаки открыта кожа, тем опаснее ей находиться на солнце. ' +
-          'Даже краткосрочное пребывание под прямыми солнечными лучами может спровоцировать сильные ожоги. ' +
-          'Другие, не такие опасные, но неприятные последствия – это дерматит, сухость, перхоть.',
-        category: 'Dog'
-      },
-      {
-        id: 3,
-        title: 'Статья про туман',
-        description: 'Эх... туман',
-        category: 'Fog'
-      }
-    ];
-
+    const mas = JSON.parse(localStorage.getItem('Articles'));
+    delete mas[id - 1];
+    localStorage.setItem('Articles', JSON.stringify(mas.filter(item => !!item)));
+    setItems(JSON.parse(localStorage.getItem('Articles')))
+  }
 
   const styleLink = 'text-decoration-none text-dark d-block col-11'
 
+  //восстановить локал сторэдж
+  // localStorage.setItem('Articles', JSON.stringify(articles));
+  // console.log(itemsMas)
+
   useEffect(() => {
-    fetchItems();
-  }, []);
+    setItems(JSON.parse(localStorage.getItem('Articles')))
+  }, [])
 
-
-  const fetchItems = async () => {
-    // const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
-    // const items = await data.json();
-    // console.log(items.data)
-    // setItems(items.data);
-    setItems(articles);
-  };
-
-  //get current articles
   const indexOfLastArticle = currentPage * articlesPerPage;
   const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
   const currentArticles = itemsMas.slice(indexOfFirstArticle, indexOfLastArticle);
 
-  //change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
