ec45daa584c6db53814ecc28f531be6a296a3b30
dannotdead <daniil.koka2000@gmail.com>
2021-07-14 01:38:51 +0700

    post + put запросы работают, ура))))

--- src/components/workWithContent/ArticleDetail.js
+++ src/components/workWithContent/ArticleDetail.js
@@ -10,43 +10,57 @@ const ArticleDetail = (props) => {
   const url = 'http://localhost:8000/api/content/contents/';
   const data = {
     "id": 1,
-    "title": "Шестая статья",
+    "title": "Вторая статья",
     "description": "Первая отправленая статья",
     "userId": 1,
-    "publishedAt": "2021-07-01T10:38:01+0000",
-    "modifiedAt": "2021-07-01T10:38:01+0000",
-    "categories": [
-      {
-        "id": 1,
-        "name": "Технологии"
-      }
-    ]
+    "categories": [1],
+    "content": "asldfkjjsdlkj123"
   };
 
   useEffect(() => {
-    fetchItem();
     getCategories()
+    // postItem()
+    // putItem()
     getContent()
   }, []);
 
-  const fetchItem = async () => {
-    await fetch(url, {
-      method: "POST",
-      body: JSON.stringify(data),
-      //make sure to serialize your JSON body
-      headers: {
-        "YT-AUTH-TOKEN": "YourTar 878b9c2d1b9eb1e5cbb140b2cf756ae323ad91ac0aba06a5d66652af77cfa5c7eb247d7be0c86c02557b6bb0f0f7f139abadd76df4a23be3f17f2ffc15806226",
-        "Content-Type": "application/json",
-      }
-    })
-      .then(response => {
-        response.json()
-        // console.log(response)
-      })
-      .then(body => {
-        console.log(body);
-      });
-  }
+  // const postItem = async () => {
+  //   await fetch(url, {
+  //     method: "POST",
+  //     body: JSON.stringify(data),
+  //     //make sure to serialize your JSON body
+  //     headers: {
+  //       "YT-AUTH-TOKEN": "YourTar 878b9c2d1b9eb1e5cbb140b2cf756ae323ad91ac0aba06a5d66652af77cfa5c7eb247d7be0c86c02557b6bb0f0f7f139abadd76df4a23be3f17f2ffc15806226",
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //     .then(response => {
+  //       response.json()
+  //       console.log(response)
+  //     })
+  //     .then(body => {
+  //       console.log(body);
+  //     });
+  // }
+
+  // const putItem = async () => {
+  //   await fetch(url, {
+  //     method: "PUT",
+  //     body: JSON.stringify(data),
+  //     //make sure to serialize your JSON body
+  //     headers: {
+  //       "YT-AUTH-TOKEN": "YourTar 878b9c2d1b9eb1e5cbb140b2cf756ae323ad91ac0aba06a5d66652af77cfa5c7eb247d7be0c86c02557b6bb0f0f7f139abadd76df4a23be3f17f2ffc15806226",
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //     .then(response => {
+  //       response.json()
+  //       console.log(response)
+  //     })
+  //     .then(body => {
+  //       console.log(body);
+  //     });
+  // }
 
   const getCategories = async () => {
     await fetch(`http://localhost:8000/api/content/categories/`)
