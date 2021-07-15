0b4f6282b5041a88fdb261740cc86cb70ed46d1c
dannotdead <daniil.koka2000@gmail.com>
2021-07-15 13:03:18 +0700

    edit content

--- src/components/workWithContent/EditContent.js
+++ src/components/workWithContent/EditContent.js
@@ -1,62 +1,107 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Form} from "react-bootstrap";
 import {useHistory} from "react-router-dom";
 
 const EditContent = (props) => {
   const history = useHistory()
 
+  const [articleMas, setArticle] = useState([]);
+  const [categoryMas, setCategories] = useState([]);
+
+  const url = `http://localhost:8000/api/content/contents/${props.match.params.id}`
   //Вместо этого будут get запросы
-  const articlesMas = JSON.parse(localStorage.getItem('Articles'));
-  const categoriesMas = JSON.parse(localStorage.getItem('Categories'));
+  // const articlesMas = JSON.parse(localStorage.getItem('Articles'));
+  // const categoriesMas = JSON.parse(localStorage.getItem('Categories'));
 
   //функция подстановки нужного параметра в форму
-  const checkDefaultValue = (parametr) => {
-    let n;
-    articlesMas.map(item => {
-      if (item.id == props.match.params.id){
-        n = item[parametr]
+  // const checkDefaultValue = (parametr) => {
+  //   let n;
+  //   articlesMas.map(item => {
+  //     if (item.id == props.match.params.id){
+  //       n = item[parametr]
+  //     }
+  //   })
+  //   return n;
+  // }
+
+  // console.log(articlesMas, props.match.params.id)
+
+  const putItem = async (data) => {
+    await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      //make sure to serialize your JSON body
+      headers: {
+        "YT-AUTH-TOKEN": "YourTar 878b9c2d1b9eb1e5cbb140b2cf756ae323ad91ac0aba06a5d66652af77cfa5c7eb247d7be0c86c02557b6bb0f0f7f139abadd76df4a23be3f17f2ffc15806226",
+        "Content-Type": "application/json"
       }
     })
-    return n;
+    .then(response => {
+      response.json()
+    })
+    .then(body => {
+    });
   }
-
-  console.log(articlesMas, props.match.params.id)
 
   const createArticleOnClick = (event) => {
-    // ВМЕСТО ЛОКАЛ СТОРЭДЖ НАДО ЗАПРОСЫ НА СЕРВ ОТПРАВЛЯТЬ
-    event.preventDefault()
-    let mas1 = {
-      id: parseInt(props.match.params.id),
-      title: event.target[0].value,
-      category: event.target[1].value,
-      description: event.target[2].value,
-    }
-    delete articlesMas[parseInt(props.match.params.id)];
-    articlesMas.splice(mas1.id - 1, 0, mas1)
-    console.log(articlesMas)
-    // localStorage.setItem('Articles', JSON.stringify(articlesMas))
+    const data = {
+      "id": 1,
+      "title": event.target[0].value,
+      "description": event.target[2].value,
+      "userId": 1,
+      "categories": [parseInt(event.target[1].value)],
+      "content": "asldfkjjsdlkj123"
+    };
+    putItem(data)
     history.push(`/content/articles`)
   }
+
+  useEffect(() => {
+    getArticles()
+    getCategories()
+  }, [])
+
+  const getArticles = async () => {
+    await fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticle(data);
+      });
+  }
+
+  const getCategories = async () => {
+    await fetch(`http://localhost:8000/api/content/categories/`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCategories(data.data);
+      });
+  }
+
+  console.log(articleMas, categoryMas)
 
   return (
     <Form onSubmit={createArticleOnClick}>
       <Form.Group controlId="formBasicArticle">
         <Form.Label>Заголовок</Form.Label>
-        <Form.Control type="text" defaultValue={checkDefaultValue('title')}/>
+        <Form.Control type="text" defaultValue={articleMas.title}/>
       </Form.Group>
 
       <Form.Group controlId="exampleForm.ControlSelect">
         <Form.Label>Выбрать категорию</Form.Label>
-        <Form.Control as="select" defaultValue={checkDefaultValue('category')}>
-          {categoriesMas.map(item => (
-            <option>{item.title}</option>
+        <Form.Control as="select">
+          {categoryMas.map(item => (
+            <option key={item.id}>{item.id} {item.name}</option>
           ))}
         </Form.Control>
       </Form.Group>
 
       <Form.Group controlId="formBasicCategory">
         <Form.Label>Добавить описание</Form.Label>
-        <Form.Control as="textarea" defaultValue={checkDefaultValue('description')}/>
+        <Form.Control as="textarea" defaultValue={articleMas.description}/>
       </Form.Group>
 
       <Button variant="primary" type="submit">
